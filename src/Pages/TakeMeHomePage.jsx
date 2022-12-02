54a6ba85608859a91182864ed487745262a79e84
orpd0523 <orpd0523@gmail.com>
2022-12-02 12:36:33 -0600

    changed image for home page

--- src/Pages/TakeMeHomePage.jsx
+++ src/Pages/TakeMeHomePage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Clock from "../components/Clock/Clock";
 import Button from "../components/Button/Button";
 import Card from "../components/Card/Card";
+import myHouse from "../assets/images/Image-My House.png"
 
 const TakeMeHomePage = () => {
   return (
@@ -12,7 +13,7 @@ const TakeMeHomePage = () => {
           <p className="typography typography--h3 typography--center">Walking home from the grocery store</p>
         </Card>
         <Card
-          src="https://www.realestate.com.au/blog/images/800x450-fit,progressive,format=webp/2017/06/12121155/capi_5598f67b37628b0e689d4b88ded2e15f_de2ee4cbfc7de746762b45dd42c90879.jpeg"
+          src={myHouse}
           alt="home"
         >
           <p className="typography typography--purple typography--h3 typography--center">YOUR ADDRESS IS</p>
--- src/Pages/YouAreHomePage.jsx
+++ src/Pages/YouAreHomePage.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Clock from "../components/Clock/Clock";
 import Card from "../components/Card/Card";
+import myHouse from "../assets/images/Image-My House.png"
 
 const TakeMeHomePage = () => {
   return (
     <>
       <Clock />
         <Card
-          src="https://www.realestate.com.au/blog/images/800x450-fit,progressive,format=webp/2017/06/12121155/capi_5598f67b37628b0e689d4b88ded2e15f_de2ee4cbfc7de746762b45dd42c90879.jpeg"
+          src={myHouse}
           alt="home" audio={true} location={false}
         >
           <p className="typography typography--purple typography--h3 typography--center">YOUR ARE</p>
