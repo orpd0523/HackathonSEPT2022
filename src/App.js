48820c4cb1d7a1b1eb122eae1236a1fbbbca8dfe
armie95 <115040104+armie95@users.noreply.github.com>
2022-12-02 17:50:53 -0500

    removed unused comments

--- src/App.js
+++ src/App.js
@@ -12,21 +12,6 @@ import { RiArrowGoBackFill } from "react-icons/ri";
 import { useEffect, useRef } from "react";
 
 function App() {
-  // let i = useRef(1);
-  // let id = null;
-  // const startRoute = () => {
-  //   id = setInterval(() => {
-  //     window.location = `/path/${i.current}`;
-  //     i.current++;
-  //     if (i.current > 4) {
-  //       clearInterval(id);
-  //     }
-  //   }, 3000);
-  // };
-
-  // useEffect(() => {
-  //   startRoute();
-  // }, []);
   return (
     <div className="container">
       <BrowserRouter>
