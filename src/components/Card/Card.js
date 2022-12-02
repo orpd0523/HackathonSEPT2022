412ba176616aaff5523965f4375c27530a4f8321
orpd0523 <orpd0523@gmail.com>
2022-12-02 01:17:09 -0600

    card component

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,13 +1,18 @@
-import "./Card.scss"
+import "./Card.scss";
 
-function Card(props){
-    const { children, src="#", alt="#" } = props;
-    return(
-        <div className="card">
-            <div className="card__top card__top--small"><img className="card__image" src={src} alt={alt}/></div>
-            <div className="card__bottom">{children}</div>
+function Card(props) {
+  const { children, src, alt } = props;
+  return (
+    <div className="card">
+      {src && (
+        <div className="card__top card__top--small">
+          <img className="card__image" src={src} alt={alt} />
         </div>
-    )
+      )}
+      {/*If src is passed down in prop(exists) true, then render card__top*/}
+      <div className="card__bottom">{children}</div>
+    </div>
+  );
 }
 
 export default Card;
