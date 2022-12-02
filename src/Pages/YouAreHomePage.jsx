313237b1a77a9b0249ee920869ab9c8fd22fedcd
orpd0523 <orpd0523@gmail.com>
2022-12-02 03:14:20 -0600

    did location and audio

--- src/Pages/YouAreHomePage.jsx
+++ src/Pages/YouAreHomePage.jsx
@@ -8,7 +8,7 @@ const TakeMeHomePage = () => {
       <Clock />
         <Card
           src="https://www.realestate.com.au/blog/images/800x450-fit,progressive,format=webp/2017/06/12121155/capi_5598f67b37628b0e689d4b88ded2e15f_de2ee4cbfc7de746762b45dd42c90879.jpeg"
-          alt="home" audio={true}
+          alt="home" audio={true} location={false}
         >
           <p className="typography typography--purple typography--h3 typography--center">YOUR ARE</p>
           <p className="typography typography--h3 typography--center">Home {":)"}
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,22 +1,32 @@
 import "./Card.scss";
-import audioIcon from '../../assets/icons/Icon-audio.svg';
+import audioIcon from "../../assets/icons/Icon-audio.svg";
+import locationIcon from "../../assets/icons/Icon-location.svg";
 
 function Card(props) {
-  const { children, src, alt, audio=false } = props;
+  const { children, src, alt, audio = false, location = false } = props;
   return (
     <div className="card">
       {src && (
         <div className="card__top card__top--small">
+          {location && (
+            <div className="card__location">
+              <img className="" src={locationIcon} alt="location" />
+              <div>
+                <p className="typography typography--body2">YOUR ADDRESS:</p>
+              <p className="typography typography--body2">234 w 17 ave</p>
+            </div>
+            </div>
+          )}
           <img className="card__image" src={src} alt={alt} />
         </div>
       )}
       {/*If src is passed down in prop(exists) true, then render card__top*/}
       <div className="card__bottom">
-        {children} 
+        {children}
         {audio && <img className="card__audio" src={audioIcon} alt="audio" />}
       </div>
     </div>
-  );
+  )
 }
 
 export default Card;
